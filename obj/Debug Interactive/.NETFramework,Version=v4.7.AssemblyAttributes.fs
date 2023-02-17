4158f5bb31492d320c261545209d2988cb24a99a
Leila Dardouri <leila.dardouri@outlook.it>
2023-02-17 15:31:20 +0100

    removed stuff

--- obj/Debug Interactive/.NETFramework,Version=v4.7.AssemblyAttributes.fs
+++
@@ -1,3 +0,0 @@
-namespace Microsoft.BuildSettings
-                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.7", FrameworkDisplayName=".NET Framework 4.7")>]
-                do ()
